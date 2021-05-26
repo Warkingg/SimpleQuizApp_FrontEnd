7a168b3ace8312112f257a38e2c0e58ac927594f
Tam250399 <tamthanhsk25@gmail.com>
2021-05-26 14:39:28 +0700

    add-forgot-password

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ForgotPasswordService} from '../service/forgot-password.service';
+import {Router} from '@angular/router';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
-
-  constructor() { }
+  forgotPasswordForm: FormGroup = new FormGroup({
+    email: new FormControl('')
+  });
+  isSubmitted = false;
+  constructor(private forgotPassword: ForgotPasswordService,
+              private router : Router) { }
 
   ngOnInit() {
   }
-
+  passwordForgot() {
+    this.isSubmitted = true;
+    this.forgotPassword.passwordForgot(this.forgotPasswordForm.value).subscribe(() => {
+      this.forgotPasswordForm.reset();
+      alert("thành công")
+    }, () => {
+      alert("thất bại")
+    });
+  }
 }
